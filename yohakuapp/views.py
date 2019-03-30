83dbd5886189fd3abcd1d3d4ec4cac4f5484a99f
Osa <osaimola@gmail.com>
2019-03-30 06:06:12 +0100

    delete migrations

--- yohakuapp/views.py
+++ yohakuapp/views.py
@@ -10,38 +10,40 @@ import os
 import tweepy as tweepy
 
 # Create your views here.
-#TODO hide api key
-CONSUMER_KEY = os.environ['CONSUMER_KEY']
-CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
-ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
-ACCESS_SECRET = os.environ['ACCESS_SECRET']
+# TODO hide api key
+#CONSUMER_KEY = os.environ['CONSUMER_KEY']
+#CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+#ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+#ACCESS_SECRET = os.environ['ACCESS_SECRET']
 
 
 def setTwitterAuth():
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
-    api = tweepy.API(auth)
-    return api
+    #auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    #auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+    #api = tweepy.API(auth)
+    #return api
+    pass
 
 
 def makeTweet(api, tweet):
-    #TODO add code to create threads for tweets greater than 280 chars
+    # TODO add code to create threads for tweets greater than 280 chars
     api.update_status(tweet)
 
+
 def index(request):
-    context ={}
+    context = {}
     if request.method == 'POST':
         form = TweetForm(request.POST)
 
         if form.is_valid():
             pending_tweet = form.save(commit=False)
 
-            #TODO handle tweets longer than 280 characters and update tweet model to reflect
+            # TODO handle tweets longer than 280 characters and update tweet model to reflect
             api = setTwitterAuth()
             makeTweet(api, pending_tweet.tweet_content)
-            pending_tweet.publish_status=True
+            pending_tweet.publish_status = True
 
-            #TODO update anonymous USERID and save to pending tweet
+            # TODO update anonymous USERID and save to pending tweet
             pending_tweet.save()
             return HttpResponseRedirect(reverse('yohakuapp:index'))
         else:
