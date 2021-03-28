abba353f3bdb2b1ce307d071f4bd5e08231a2d12
Osa <osaimola@gmail.com>
2021-03-28 17:04:12 -0600

    easier search result clearing

--- static/yohakuapp/scripts.js
+++ static/yohakuapp/scripts.js
@@ -8,6 +8,9 @@ const close_form = document.getElementById("close_form");
 
 const search_icon = document.getElementById("search_icon");
 const search_field = document.getElementById("search");
+const clear_results = document.getElementById("clear_results");
+
+let clickedElement = null;
 
 open_about.addEventListener("click", () => {
   about_overlay.classList.remove("hidden");
@@ -25,7 +28,21 @@ close_form.addEventListener("click", () => {
 
 search_field.addEventListener("focus", () => {
   search_icon.classList.add("rotate-45");
+  if (clear_results) {
+    clear_results.classList.remove("sr-only");
+    clear_results.classList.add("translate-y-3.5");
+  }
 });
-search_field.addEventListener("blur", () => {
+
+search_field.addEventListener("blur", (e) => {
   search_icon.classList.remove("rotate-45");
+  if (clear_results && clickedElement != clear_results) {
+    // hide clear results button only when a different element is clicked to lose focus
+    clear_results.classList.remove("translate-y-3.5");
+    clear_results.classList.add("sr-only");
+  }
+});
+
+document.addEventListener("mousedown", (e) => {
+  clickedElement = e.target;
 });
